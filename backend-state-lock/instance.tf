2c27c7d01953d6a758e724565da4c04b997b8bba
demo@.gamil.com <rajput.vishal.rajesh@gmail.com>
2024-10-04 12:15:27 +0530

    backent-state-lock

---
+++ backend-state-lock/instance.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "s3" {
+    bucket = "mybucket123456789"
+    region = "ap-south-1"
+    key = "terraform.tfstate"
+    dynamodb_table = "my-state-lock-table"
+    
+  }
+}
+
+resource "aws_instance" "second_instance" {
+    ami = "ami-0dee22c13ea7a9a67"
+    instance_type = "t2.micro"
+}
